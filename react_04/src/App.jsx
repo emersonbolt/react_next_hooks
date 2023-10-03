b5781298c0fe6e320700e9ca9ef6bee2134c96bf
Emerson Teixeira <emersonteixeira@Mac-Studio-de-Emerson.local>
2023-10-02 23:34:20 -0300

    hook useState 01

---
+++ react_04/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HookState01 from './components/UseState_01'
+export default () => {
+
+    return (
+        <>
+            <h1>REACT HOOKS</h1>
+            <HookState01 />
+        </>
+    )
+}
